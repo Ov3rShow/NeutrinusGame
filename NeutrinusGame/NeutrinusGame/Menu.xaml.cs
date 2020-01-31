6b0d4ee5d436239b1a765b1b6d4d888f59324b4b
Giorgia Giacomazzo <giorgia.giaco@gmail.com>
2020-01-31 14:18:48 +0100

    Aggiunta pagina menu

---
+++ NeutrinusGame/NeutrinusGame/Menu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NeutrinusGame
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_newGame(object sender, EventArgs e)
+        {
+           Navigation.PushModalAsync(new GamePage());
+        }
+    }
+}
